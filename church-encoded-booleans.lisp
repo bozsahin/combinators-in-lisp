72fef6990b245b69490c48213e2a39ca020de0ef
bozsahin <cem.bozsahin@gmail.com>
2018-09-13 19:49:36 +0300

    eliminating the combinator reader macro

--- church-encoded-booleans.lisp
+++ church-encoded-booleans.lisp
@@ -1,7 +1,8 @@
 ;; this is an example to implement Church booleans 
 ;; load the file and do (comb-eval myor true false) to get (K K) viz \y\x\y2.x
+;; also try (comb-eval myor false false) which should give you K (false)
 ;; the example is borrowed from Steve Piantadosi
 
-(setf myor '(#&s #&s (#&k (#&k #&k))))
-(setf true '(#&k #&k))
-(setf false '#&k)
+(setf myor `(,ss ,ss (,k (,k ,k))))
+(setf true `(,k ,k))
+(setf false `,k)
