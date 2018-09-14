25c21793440feef9f2378b2e4360992d05c37163
bozsahin <cem.bozsahin@gmail.com>
2018-09-14 13:29:10 +0300

    on to church-encoding of any n

--- church-encoded-numerals-with-y.lisp
+++ church-encoded-numerals-with-y.lisp
@@ -1,5 +1,5 @@
 ;; this is an example to show Y combinator in action
-;; load the file and do (comb-eval '#&y ch-h ch-3) to get factorial of 3
+;; load the file and do (comb-eval `,y ch-h ch-3) to get factorial of 3
 ;;
 ;; Factorial is functionally FAC n = H FAC n, where H=\f\n.(if (= n 0) 1 (* n f (- n 1)))
 ;;   therefore FAC = Y H
@@ -16,7 +16,7 @@
 (defconstant  ch-5 '(lam f (lam x (f (f (f (f (f x))))))))
 (defconstant  ch-6 '(lam f (lam x (f (f (f (f (f (f x)))))))))
 (defconstant  ch-mult '(lam m (lam n (lam f (lam x #$(m (n f) x))))))
-(defconstant  ch-pred '(lam n (lam f (lam x #$(n (lam g (lam h (h (g f)))) (lam u x) (lam u u))))))
+(defconstant  ch-pred '(lam n (lam f (lam x #$(n (lam g (lam h (h (g f))) (lam u x) (lam u u)))))))
 (defconstant  ch-minus `(lam m (lam n #$(n ,ch-pred m))))
 (defconstant  ch-if '(lam p (lam a (lam b #$(p a b)))))
 (defconstant  ch-false '(lam a (lam b b)))
@@ -25,4 +25,4 @@
 (defconstant  ch-and '(lam p (lam q #$(p q p))))
 (defconstant  ch-leq `(lam m (lam n #$(,ch-is0 (,ch-minus m n)))))
 (defconstant  ch-eq `(lam m (lam n #$(,ch-and (,ch-leq m n) (,ch-leq n m)))))
-(defconstant  ch-h `(lam f (lam n #$(,ch-if (,ch-eq n ,ch-0) ,ch-1 (,ch-mult n f (,ch-minus n ,ch-1))))))
+(defconstant  ch-h `(lam f (lam n #$(,ch-if (,ch-eq n ,ch-0) ,ch-1 (,ch-mult n (f (,ch-minus n ,ch-1)))))))
